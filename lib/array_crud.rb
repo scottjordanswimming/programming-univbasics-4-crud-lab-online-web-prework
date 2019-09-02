73fc0627fda31d36936486bc37e03b59dc21a429
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-02 18:21:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,12 +14,12 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift(create_an_array, "wow")
+
   
 end
 
 def remove_element_from_end_of_array(array)
-create_an_array.pop
+
   
 end
 
